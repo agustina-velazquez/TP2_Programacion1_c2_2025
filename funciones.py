978701336afcd6723982f8f624536c9517e27cb2
Agustina Velázquez <velazquezagus00@gmail.com>
2025-11-26 03:53:23 -0300

    Logica de dados en proceso

---
+++ funciones.py
@@ -0,0 +1,63 @@
+import random
+
+
+
+def tirar_dados(numero_de_dados, cara_inicial, cara_final):
+
+    lista_auxiliar = []
+   
+    for i in range(numero_de_dados):
+        
+        lista_auxiliar.append(random.randint(cara_inicial, cara_final))
+    
+    print(lista_auxiliar)
+
+    
+    return lista_auxiliar
+        
+
+
+def elegir_dados(lista_auxiliar):
+
+    dados_elegidos = []
+    
+    for i in range(len(lista_auxiliar)):
+        posicion = input("Ingrese posicion que desea conservar, o ENTER para no conservar")
+
+        if not posicion.isdigit():
+
+            return dados_elegidos
+
+        dados_elegidos.append(lista_auxiliar[int(posicion)])
+        print(dados_elegidos)
+
+    return dados_elegidos
+        
+
+def armar_lista_final(tiros,numero_de_dados,cara_inicial,cara_final):
+    lista_final = []
+    lista_auxiliar = []
+    dados_elegidos = []
+
+    while tiros > 0:
+
+        lista_auxiliar = tirar_dados(numero_de_dados, cara_inicial, cara_final)
+        dados_elegidos = elegir_dados(lista_auxiliar)
+        numero_de_dados = numero_de_dados - len(dados_elegidos)
+        lista_final.extend(dados_elegidos)
+        tiros = tiros - 1
+
+        if tiros == 1:
+            lista_final.extend(lista_auxiliar)
+            tiros = tiros - 1
+
+    return lista_final
+   
+print(armar_lista_final(3, 5, 1, 6))
+
+
+
+
+
+
+
