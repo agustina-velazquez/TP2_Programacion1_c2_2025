f9c13b69c812ccab55c3d550c42af29f0ca1bc26
Agustina Velázquez <velazquezagus00@gmail.com>
2025-11-28 10:09:13 -0300

    se eligen bien los dados ahora

--- funciones.py
+++ funciones.py
@@ -10,7 +10,7 @@ def tirar_dados(numero_de_dados, cara_inicial, cara_final):
         
         lista_auxiliar.append(random.randint(cara_inicial, cara_final))
     
-    print(lista_auxiliar)
+    print(f"tirada de dados: {lista_auxiliar}")
 
     
     return lista_auxiliar
@@ -22,14 +22,14 @@ def elegir_dados(lista_auxiliar):
     dados_elegidos = []
     
     for i in range(len(lista_auxiliar)):
-        posicion = input("Ingrese posicion que desea conservar, o ENTER para no conservar")
+        posicion = input("Ingrese posicion que desea conservar, o ENTER para no conservar: ")
 
         if not posicion.isdigit():
 
             return dados_elegidos
 
         dados_elegidos.append(lista_auxiliar[int(posicion)])
-        print(dados_elegidos)
+        print(f"los elegidos son: {dados_elegidos}")
 
     return dados_elegidos
         
@@ -47,10 +47,24 @@ def armar_lista_final(tiros,numero_de_dados,cara_inicial,cara_final):
         lista_final.extend(dados_elegidos)
         tiros = tiros - 1
 
-        if tiros == 1:
-            lista_final.extend(lista_auxiliar)
-            tiros = tiros - 1
+    while len(lista_final) < 5:
+        print("Debe elegir mas dados.")
+        
 
+        if dados_elegidos == []:
+            dados_elegidos = elegir_dados(lista_auxiliar)
+            lista_final.extend(dados_elegidos)
+        
+        a = set(lista_auxiliar)
+        b = set(dados_elegidos)
+        restantes = a.difference(b)
+        print(f"los restantes son: {list(restantes)}")
+
+        dados_elegidos = elegir_dados(list(restantes))
+       
+        lista_final.extend(dados_elegidos)
+        
+            
     return lista_final
    
 print(armar_lista_final(3, 5, 1, 6))
